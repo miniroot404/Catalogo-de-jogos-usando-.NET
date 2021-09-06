21c390ff25f3e73748e0a069ba7fc61aa69ae202
miniroot404 <brunna.alanna@gmail.com>
2021-09-06 18:35:13 -0300

    projeto Criando catálogo de jogo com .NET

---
+++ ApiCatalogoJogos/Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Entities
+{
+    public class JogoNaoCadastradoException : Exception
+    {
+
+        public JogoNaoCadastradoException()
+            : base("Este Jogo não está cadastrado.")
+        { }
+    }
+}
